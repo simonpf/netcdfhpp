1307d83f6a6adf91d897d1ebe710f3ae545b3d7d
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-09-05 16:34:24 +0200

    Renamed to netcdfpp for github compatibility.

--- test/test_create_and_open.cxx
+++ test/test_create_and_open.cxx
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
-#include <netcdf++.h>
+#include <netcdfpp.h>
 
 TEST_CASE( "create_and_open_file", "[factorial]" ) {
     auto file = netcdf4::File::create("test_file.nc");
